cdafe9a86bec1d85203abd2f09b6e53fae4f4f53
sarveshhome <sarvesh.carlton@gmail.com>
2021-12-29 03:15:19 +0000

    1 file modified

--- src/reducers/SongList.js
+++ src/reducers/SongList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongList = () => {
+const SongList = (props) => {
+  console.log('song list components ');
+  console.log(props);
   return <div>SongList</div>;
 };
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
