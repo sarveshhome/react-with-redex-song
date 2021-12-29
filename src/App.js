1919c4c8f0e0d680b916ba17f8744c46417aa62d
sarveshhome <sarvesh.carlton@gmail.com>
2021-12-29 02:45:35 +0000

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './style.css';
 import SelectSong from './actions';
+import SongList from './reducers/SongList.js';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 }
